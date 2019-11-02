2ff5feebcce6e1f76e2fad1ca9878a9449e68825
blito <pablomisfit@gmail.com>
2019-11-02 12:07:22 -0300

    some changes

--- demo.js
+++ demo.js
@@ -1,1 +1,12 @@
-console.log("hola!");
+function saludar(nombre){
+  console.log("hola: "+nombre);
+}
+
+//window.onload = function() {
+//  console.log("hola on load window");
+  var nombre="Mauro";
+  var apellido="Yapura";
+//  console.log(nombre+' '+apellido);
+//};
+
+saludar(nombre);
